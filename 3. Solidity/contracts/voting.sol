6e164dd0a36d2771571ed083d9d1b0764301d83e
julie-ramadanoski <ramadanoski.j@gmail.com>
2022-10-17 22:48:39 +0200

    TP(solidity): Voting testing pack

---
+++ 3. Solidity/contracts/voting.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint256 votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint256 voteCount;
+    }
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(
+        WorkflowStatus previousStatus,
+        WorkflowStatus newStatus
+    );
+    event ProposalRegistered(uint256 proposalId);
+    event Voted(address voter, uint256 proposalId);
+
+    uint256 public winningProposalId;
+    WorkflowStatus public status;
+    Proposal[] public proposals;
+    mapping(address => Voter) public voters;
+
+    modifier onlyRegistered() {
+        require(voters[msg.sender].isRegistered, "you are not registered");
+        _;
+    }
+
+    constructor() Ownable() {}
+
+    function registerVoter(address _address) public onlyOwner {
+        require(
+            status == WorkflowStatus.RegisteringVoters,
+            "you are allowed to Register Voters only at RegisteringVoters session"
+        );
+
+        voters[_address] = Voter(true, false, 0);
+
+        emit VoterRegistered(_address);
+    }
+
+    function changeStatus(WorkflowStatus _status) public onlyOwner {
+        emit WorkflowStatusChange(status, _status);
+        status = _status;
+    }
+
+    function registerProposal(string memory _description)
+        public
+        onlyRegistered
+    {
+        require(
+            status == WorkflowStatus.ProposalsRegistrationStarted,
+            "you are allowed to add Proposals only at ProposalsRegistrationStarted session"
+        );
+
+        proposals.push(Proposal(_description, 0));
+
+        emit ProposalRegistered(proposals.length - 1);
+    }
+
+    function registerVote(uint256 _proposalId) public onlyRegistered {
+        require(
+            status == WorkflowStatus.VotingSessionStarted,
+            "you are allowed to Vote only at VotingSessionStarted session"
+        );
+        require(!voters[msg.sender].hasVoted, "you have already voted");
+
+        proposals[_proposalId].voteCount++;
+        voters[msg.sender].hasVoted = true;
+
+        emit Voted(msg.sender, _proposalId);
+    }
+
+    /*
+     * La proposition gagnante est celle qui possède le plus de votes
+     * Afin de favoriser l'action à la réflexion, s'il y a égalité
+     * le premier à avoir fait sa proposition est déclaré vainqueur
+     */
+    function countingVotes() public onlyOwner {
+        require(
+            status == WorkflowStatus.VotingSessionEnded,
+            "you are allowed to count only at VotingSessionEnded session."
+        );
+
+        uint256 i = 0;
+        uint256 leaderCount = 0;
+        uint256 leaderCountId = 0;
+        for (i; i < proposals.length; i++) {
+            if (proposals[i].voteCount > leaderCount) {
+                leaderCount = proposals[i].voteCount;
+                leaderCountId = i;
+            }
+        }
+
+        winningProposalId = leaderCountId;
+    }
+
+    function getWinnerDetails()
+        external
+        view
+        onlyRegistered
+        returns (string memory)
+    {
+        require(
+            status == WorkflowStatus.VotesTallied,
+            "you are allowed to see winner details only at VotesTallied session."
+        );
+        return proposals[winningProposalId].description;
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
---
+++ 3. Solidity/test/voting.js
@@ -0,0 +1,210 @@
+const Voting = artifacts.require("Voting.sol");
+const truffleAssert = require('truffle-assertions');
+
+contract("Voting", function (accounts) {
+    userAdmin = accounts[0];
+    user1 = accounts[1];
+    user2 = accounts[2];
+    user3 = accounts[3];
+    noWhitlistedUser = accounts[4];
+
+    let voting;
+    beforeEach('should setup the contract instance', async () => {
+        voting = await Voting.deployed();
+    });
+
+    /**
+     *     L'administrateur du vote enregistre une liste blanche d'électeurs identifiés par leur adresse Ethereum.
+     */
+    it("should assert initial state empty", async function () {
+        const contractOwner = await voting.owner();
+        const status = await voting.status();
+        const winningProposalId = await voting.winningProposalId();
+        const voter1 = await voting.voters(user1);
+
+        assert.equal(contractOwner, userAdmin, "le propriétaire doit etre l'administrateur");
+        assert.equal(status.toNumber(), 0, "le statut initial doit etre l'enregistrement des votants");
+        assert.equal(winningProposalId.toNumber(), 0, "le gagnant ne doit pas etre designé");
+        assert.equal(voter1.isRegistered, false, "un visiteur ne doit pas avoir le droit de participer");
+
+    });
+
+    it("should revert on non admin user register a voter", async function () {
+        await truffleAssert.reverts(voting.registerVoter(user2, {
+            'from': user1
+        }));
+    });
+
+    it("should assert adding new voters by admin", async function () {
+        await voting.registerVoter(user1, { from: userAdmin });
+        await voting.registerVoter(user2, { from: userAdmin });
+        await voting.registerVoter(accounts[5], { from: userAdmin });
+        await voting.registerVoter(accounts[6], { from: userAdmin });
+        await voting.registerVoter(accounts[7], { from: userAdmin });
+        await voting.registerVoter(accounts[8], { from: userAdmin });
+        await voting.registerVoter(accounts[9], { from: userAdmin });
+        const result = await voting.registerVoter(user3, { from: userAdmin });
+        truffleAssert.eventEmitted(result, 'VoterRegistered');
+
+        const voter1 = await voting.voters(user1);
+        assert.equal(voter1.isRegistered, true, "le votant doit pourvoir participer");
+
+    });
+    /**
+     * L'administrateur du vote commence la session d'enregistrement de la proposition.
+     */
+    it("should revert on non admin change status", async function () {
+        await truffleAssert.reverts(voting.changeStatus(1, {
+            'from': user1
+        }));
+    });
+
+    it("should assert admin can change status", async function () {
+        const result = await voting.changeStatus(1, {
+            'from': userAdmin
+        });
+        truffleAssert.eventEmitted(result, 'WorkflowStatusChange');
+
+        const status = await voting.status();
+        assert.equal(status.toNumber(), 1, "Le statut doit etre passé à ProposalsRegistrationStarted");
+    });
+
+    it("should revert on non registered user add proposal", async function () {
+        await truffleAssert.reverts(voting.registerProposal("description malicieuse", {
+            'from': noWhitlistedUser
+        }));
+    });
+
+    /**
+     * Les électeurs inscrits sont autorisés à enregistrer leurs propositions pendant que la session d'enregistrement est active.
+     */
+    it("should revert on admin registered voters out of register session", async function () {
+        await truffleAssert.reverts(voting.registerVoter(user1, {
+            'from': userAdmin
+        }));
+    });
+
+    it("should assert registered users can add proposal", async function () {
+        const description = "description pertinente";
+        const result = await voting.registerProposal(description, {
+            'from': user1
+        });
+        truffleAssert.eventEmitted(result, 'ProposalRegistered');
+
+        const proposal = await voting.proposals(0);
+        assert.equal(proposal.description, description, "La description doit etre enregistrée");
+
+        // add 2 proposals
+        await voting.registerProposal("Autre proposition", {
+            'from': user2
+        });
+        await voting.registerProposal("troisième proposition", {
+            'from': user2
+        });
+    });
+
+    /**
+     * L'administrateur de vote met fin à la session d'enregistrement des propositions.
+     */
+    it("should revert on add proposals out of proposals session", async function () {
+        await voting.changeStatus(2, {
+            'from': userAdmin
+        });
+        await truffleAssert.reverts(voting.registerProposal("proposition passée hors de la session", {
+            'from': user2
+        }));
+    });
+    /**
+     * L'administrateur du vote commence la session de vote.
+     * Les électeurs inscrits votent pour leur proposition préférée.
+     */
+    it("should assert registred user can vote", async function () {
+        await voting.changeStatus(3, {
+            'from': userAdmin
+        });
+
+        // Create equality, the winner is the first to add proposal
+        const result = await voting.registerVote(0, {
+            'from': user1
+        });
+        truffleAssert.eventEmitted(result, 'Voted');
+
+        await voting.registerVote(1, {
+            'from': user2
+        });
+        await voting.registerVote(1, {
+            'from': user3
+        });
+        await voting.registerVote(2, {
+            'from': accounts[5]
+        });
+        await voting.registerVote(2, {
+            'from': accounts[6]
+        });
+        const proposal0 = await voting.proposals(0);
+        const proposal1 = await voting.proposals(1);
+        const proposal2 = await voting.proposals(2);
+        assert.equal(proposal0.voteCount, 1, "La proposition 0 doit avoir 1 vote");
+        assert.equal(proposal1.voteCount, 2, "La proposition 1 doit avoir 2 votes"); // proposition gagnante
+        assert.equal(proposal2.voteCount, 2, "La proposition 2 doit avoir 2 votes");
+    });
+
+    it("should revert on vote by no registrered user", async function () {
+        await truffleAssert.reverts(voting.registerVote(0, {
+            'from': noWhitlistedUser
+        }));
+    });
+
+    it("should revert on vote twice or more by registred user", async function () {
+        await truffleAssert.reverts(voting.registerVote(0, {
+            'from': user1
+        }));
+    });
+
+    /**
+     * L'administrateur du vote met fin à la session de vote.
+     * L'administrateur du vote comptabilise les votes.
+     */
+    it("should revert on get winner details out of VotesTallied session", async function () {
+        await truffleAssert.reverts(voting.getWinnerDetails({
+            'from': user1
+        }));
+    });
+
+    it("should revert on get winner details by non registred user", async function () {
+        await truffleAssert.reverts(voting.getWinnerDetails({
+            'from': noWhitlistedUser
+        }));
+    });
+
+    it("should revert on calculate winner out of VotingSessionEnded session", async function () {
+        await truffleAssert.reverts(voting.countingVotes({
+            'from': userAdmin
+        }));
+    });
+
+    it("should assert admin can calcule winner", async function () {
+        await voting.changeStatus(4, {
+            'from': userAdmin
+        });
+        await voting.countingVotes({
+            'from': userAdmin
+        });
+        const winningProposalId = await voting.winningProposalId();
+        assert.equal(winningProposalId.toNumber(), 1, "La proposition gagnante est la 1");
+    });
+    /**
+     *   Tout le monde peut vérifier les derniers détails de la proposition gagnante.
+     */
+    it("should assert registred user can see winner details", async function () {
+        await voting.changeStatus(5, {
+            'from': userAdmin
+        });
+        const winnerDescription = await voting.getWinnerDetails({
+            'from': user1
+        });
+        assert.equal(winnerDescription, "Autre proposition", "La description doit etre 'autre proposition'");
+
+    });
+});
+
